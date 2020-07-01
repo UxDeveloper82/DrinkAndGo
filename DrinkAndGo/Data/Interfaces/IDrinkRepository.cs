07ebd7b4a27884e861b16f2952c714378b018681
UxDeveloper82 <electronicsmf@aol.com>
2020-06-30 20:07:32 -0400

    Add Interface

---
+++ DrinkAndGo/Data/Interfaces/IDrinkRepository.cs
@@ -0,0 +1,16 @@
+﻿using DrinkAndGo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkAndGo.Data.Interfaces
+{
+    public interface IDrinkRepository
+    {
+        IEnumerable<Drink> Drinks { get; set; }
+        IEnumerable<Drink> PreferredDrinks { get; set; }
+        Drink GetDrinkById(int drinkId);
+    }
+}
